e01909caae855310c6a14e474dd6c68ccbd3b2d0
pearpiko <45189134+pearpiko@users.noreply.github.com>
2019-05-02 18:25:37 +0700

    I2C_Start - ไม่ออก loop while

--- VotingMachine2/VotingMachine2/main.c
+++ VotingMachine2/VotingMachine2/main.c
@@ -5,15 +5,20 @@
 #include <util/delay.h>			/* Include Delay header file */
 #include <string.h>
 #include <stdio.h>
-
+/*
 #define KEYPAD_DDR DDRB
 #define KEYPAD_PORT PORTB
 #define KEYPAD_PIN PINB
-
+*/
 #define LCD_Dir  DDRD			/* Define LCD data port direction */
 #define LCD_Port PORTD			/* Define LCD data port */
-#define RS PORTD2				/* Define Register Select pin */
-#define EN PORTD3 				/* Define Enable signal pin */
+#define RS PORTD0				/* Define Register Select pin */
+#define EN PORTD1 				/* Define Enable signal pin */
+
+#define SPI_DDR DDRB 
+#define SPI_MOSI PORTB3
+#define SPI_SCK PORTB5
+#define SPI_SS PORTB2
 
 int score[16] = { 0 };
 
@@ -83,7 +88,7 @@ void LCD_Clear()
 	LCD_Command (0x80);		/* Cursor at home position */
 }
 
-
+/*
 uint16_t keypad_read()
 {
 	uint16_t key = 0;
@@ -93,14 +98,15 @@ uint16_t keypad_read()
 	{
 		key = key << 4;					// shift left 4-bit
 		
-		_delay_ms(1);
-		/* scan a row and collect in key */
-		KEYPAD_DDR |= (1 << i);			// set DDRB0 to output
+		 scan a row and collect in key */
+/*		KEYPAD_DDR |= (1 << i);			// set DDRB0 to output
 		KEYPAD_PORT &= ~(1 << i);		// set to GND
+		
 		_delay_ms(1);
 		
 		key |= (KEYPAD_PIN & 0xF0) >> 4;		// read only 4-bit MSB (clear 4-bit LSB by 0)
 		
+		_delay_ms(1);
 		KEYPAD_DDR &= ~(1 << i);		// reset to input (pull up)
 		KEYPAD_PORT |= (1 << i);
 	}
@@ -113,7 +119,7 @@ void keypad_init()
 	KEYPAD_DDR = 0x00;		// set PORTB as input
 	KEYPAD_PORT = 0xFF;		// enable pull up
 }
-
+*/
 void counting(uint16_t candidate)
 {
 	score[candidate-1]++;
@@ -124,11 +130,11 @@ uint16_t voting_section()
 	LCD_Command (0x01);				// Clear display
 	LCD_String ("PLEASE VOTE.."); 	//sending string
 	LCD_Command(0xC0);				//moving courser to second line
-	uint16_t key;
+	uint16_t key=0;
 	
 	do
 	{
-		key = ~keypad_read();
+		//key = ~keypad_read();
 		key = 1 + log(key)/log(1.99);
 	} while (!key);					// wait for voting
 	
@@ -141,7 +147,6 @@ void result_section(uint16_t key)
 	LCD_Command (0x01);				// Clear display
 	LCD_String ("THANKS YOU :)");	//sending string
 	LCD_Command(0xC0);				//moving courser to second line
-
 	
 	int i = 0;
 	char buffer[20];
@@ -154,20 +159,24 @@ void result_section(uint16_t key)
 
 void I2C_Init()
 {
-	TWSR=0x00; //set presca1er bits to zero
-	TWBR=0x32; //SCL frequency is 100K for 8Mhz
-	TWCR=0x04; //enab1e TWI module
+	TWBR=0x20; //SCL frequency is 100K for 8Mhz
+	TWCR |= (1<<TWEN); //enab1e TWI module
 }
 
 void I2C_Start()
 {
-	TWCR = ((1<<TWINT) | (1<<TWSTA) | (1<<TWEN));
+	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	
+	LCD_String("32");
+	TWDR = 0xD1;	//send address (in read mode)
+	TWCR = (1<<TWINT) | (1<<TWEN);		//Ack auto
 	while (!(TWCR & (1<<TWINT)));
 }
 
 void I2C_Stop(void)
 {
-	TWCR = ((1<< TWINT) | (1<<TWEN) | (1<<TWSTO));
+	TWCR = (1<< TWINT) | (1<<TWEN) | (1<<TWSTO);
 	_delay_us(100) ; //wait for a short time
 }
 
@@ -181,12 +190,15 @@ void I2C_Write(uint8_t v_i2cData_u8)
 uint8_t I2C_ReadwithAck()
 {
 	TWCR = ((1<< TWINT) | (1<<TWEN) | (1<<TWEA));
+	LCD_String(" 12");
 	while ( !(TWCR & (1 <<TWINT)));
+	LCD_String(" 13");
 	return TWDR;
 }
 
 unsigned char RFID_Read()
 {
+	LCD_String("11");
 	uint8_t id;
 	unsigned char cardID[8];
 	int i;
@@ -211,24 +223,39 @@ unsigned char RFID_Read()
 int main(void)
 {
 	unsigned char cardID[8] ;
+	uint8_t id=0;
+	char buffer[8];
+	int i;
+	
+	DDRD |= (1<<PORTD2);
+	PORTD &= ~(1<<PORTD2);
 	
 	LCD_Init();
-	keypad_init();
+	//keypad_init();
 	I2C_Init();
-		
+	//spi_init();
+	
 	while (1)
 	{
 		LCD_Command (0x01);				// Clear display
 		LCD_String ("RFID NUMBER..");	//sending string
 		LCD_Command(0xC0);				//moving courser to second line
+		LCD_String("01");
+		I2C_Start();
+		LCD_String("02");
+		_delay_ms(1);
+		//id = I2C_ReadwithAck();
 		
-		*cardID = RFID_Read();
+		sprintf(buffer, "%u", id);
+		for (i = 0; i < strlen(buffer); i++)
+		{
+			LCD_Data(buffer[i]);					// send input character to LCD for displaying
+		}
+		//*cardID = RFID_Read();
 		
-		uint16_t key = voting_section();
-		result_section(key);
+		//uint16_t key = voting_section();
+		//result_section(key);
 		
-			
 		_delay_ms(200);
 	}
 }
-
